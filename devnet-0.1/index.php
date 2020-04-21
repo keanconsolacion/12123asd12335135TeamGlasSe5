fa05537dcd5bbc8be073d8aeff6b6c628a9b1ae2
paul-dev <201801017@iacademy.edu.ph>
2020-04-21 12:10:48 +0800

    updated to dev-0.1.2

--- devnet-0.1/index.php
+++ devnet-0.1/index.php
@@ -1,7 +1,19 @@
 <?php
+    //connection settings
+    //we use the "$conn variable from connection.php"
    include_once 'connection.php';
+
+
+      echo "$generatekey";
+      // login Function
       if(isset($_POST['submit'])){
         // header("Location:welcome.php");
+
+          //we use "mysqli_real_escape_string" to prevent any SQL Injection Attacks. not Required
+          //but neccessary
+
+          //Executing this function without a valid MySQLi connection passed in will return NULL
+          //and emit E_WARNING level errors.
           $username = mysqli_real_escape_string($conn,$_POST['username']);
           $password= mysqli_real_escape_string($conn,$_POST['password']);
 
@@ -20,6 +32,13 @@
           }
 
       }
+
+
+
+
+
+
+
  ?>
 
 <!doctype html>
@@ -44,26 +63,7 @@
       src="https://code.jquery.com/jquery-3.5.0.js"
       integrity="sha256-r/AaFHrszJtwpe+tHyNi/XCfMxYpbsRg2Uqn0x3s2zc="
       crossorigin="anonymous"></script>
-    <script>
-    // JQUERY INTERNAL
-    $(function(){
-     $(".register-border").hide() // try to hide google navigation bar
-    });
-    $(document).ready(function(){
 
-      $("#notStud").click(function(){
-
-
-        $(".register-border").fadeIn("");
-
-        // $(".register-border").show() // try to hide google navigation bar
-
-      });
-
-
-
-    });
-</script>
 <title>IHUB</title>
 
   </head>
@@ -76,11 +76,11 @@
 
 
           <form class="loginform" action="" method="post">
-            <div class="DevNet-Header">
-                <p class="text-center login_header">.devnet</p>
-            </div>
-            <div class="container mt-4">
 
+              <div class="DevNet-Header">
+                  <p class="text-center login_header">.devnet <?php echo "$generatekey"; ?></p>
+              </div>
+            <div class="container mt-4">
                 <div class="row">
                   <div class="col-1">
                     <span style="font-size: 25px;">
@@ -90,7 +90,6 @@
                   <div class="col-11">
                     <input type="text" class="form-control" id="user" name="username"placeholder="Username" required >
                   </div>
-
                 </div>
             </div>
   <div class="container mt-3">
@@ -113,7 +112,7 @@
           <div class="container mb-3">
             <div class="row">
               <div class="col">
-                <a id="notStud" href="#">Not a student?</a>
+                <a id="notStud" href="switch\switch_register.php">Not a student?</a>
               </div>
             </div>
 
@@ -129,83 +128,14 @@
           </form>
 
 
+                      </div>
                   </div>
-        </div>
-    </div>
+            </div>
       </div>
 
 
 
-      <div class="register-border" id="logindiv">
-        <div class="p-5 back">
-              <a href="">←</a>
-      </div>
-        <div class="login-text p-5">
 
-
-          <form class="regform" action="" method="post">
-            <div class="DevNet-Header">
-                <p class="text-center register_header">.devnet//guest_registration</p>
-            </div>
-            <form>
-  <div class="form-group">
-    <label for="formGroupExampleInput">First Name</label>
-    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Tim" required>
-  </div>
-  <div class="form-group">
-    <label for="formGroupExampleInput2">Middle Name</label>
-    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Berners" required>
-  </div>
-  <div class="form-group">
-    <label for="formGroupExampleInput2">Last Name</label>
-    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Lee" required>
-  </div>
-  <div class="form-group">
-    <label for="formGroupExampleInput2">Email</label>
-    <input type="email" class="form-control" id="formGroupExampleInput2" placeholder="optional">
-  </div>
-  <div class="form-group">
-    <label for="formGroupExampleInput2">Date of Visit</label>
-    <input type="date" class="form-control" id="formGroupExampleInput2" placeholder="">
-  </div>
-  <div class="form-group">
-    <label for="formGroupExampleInput2">Purpose of Visit</label>
-    <select class="custom-select">
-      <option selected>Purpose of Visit</option>
-      <option value="1">School Tour</option>
-      <option value="2">Registrar</option>
-      <option value="3">Event</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="formGroupExampleInput2">Type of ID</label>
-    <select class="custom-select">
-      <option selected>Type of ID</option>
-      <option value="1">Goverment</option>
-      <option value="2">School ID</option>
-      <option value="3">Company ID</option>
-      <option value="3">Driver License</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="formGroupExampleInput2">Contanct Number</label>
-    <input type="email" class="form-control" id="formGroupExampleInput2" placeholder="optional">
-  </div>
-
-
-          <div class="container mt-4">
-            <div class="row">
-              <div class="col text-center">
-                <input class="btn btn-outline-light" id="submit"type="submit" name="submit" value="register"style="width: 100px;">
-              </div>
-            </div>
-          </div>
-
-          </form>
-
-
-                  </div>
-        </div>
 
 
 
