4b3676a25714c8b4b5a099093d3f403f8a032887
Ke'an Consolacion <keanconsolacion@gmail.com>
2020-04-24 11:35:28 +0800

    new otso.sql

--- otso-4-24-2020.sql
+++ otso-4-24-2020.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 24, 2020 at 09:54 AM
+-- Generation Time: Apr 24, 2020 at 11:34 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -49,15 +49,42 @@ CREATE TABLE `guest_register` (
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
+  `password` varchar(50) NOT NULL,
+  `permission` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'admin');
+INSERT INTO `users` (`id`, `username`, `password`, `permission`) VALUES
+(1, 'admin', 'admin', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_school_visit`
+--
+
+CREATE TABLE `user_school_visit` (
+  `dateOfVisit` date NOT NULL,
+  `purpose` varchar(80) NOT NULL,
+  `idType` varchar(50) NOT NULL,
+  `contactNumber` int(11) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_school_visit`
+--
+
+INSERT INTO `user_school_visit` (`dateOfVisit`, `purpose`, `idType`, `contactNumber`, `id`) VALUES
+('0023-12-31', 'Enrollment', 'School ID', 23123, 1),
+('2003-12-31', 'Tour', 'Government', 12321, 2),
+('1970-01-01', 'Inquiry', 'School ID', 123, 3),
+('0023-12-31', 'Tour', 'Government', 123213, 4),
+('1970-01-01', 'Tour', 'Government', 0, 5),
+('1970-01-01', 'Inquiry', 'Company ID', 0, 6);
 
 -- --------------------------------------------------------
 
@@ -76,7 +103,7 @@ CREATE TABLE `user_token` (
 --
 
 INSERT INTO `user_token` (`id`, `username`, `token`) VALUES
-(7, 'admin', '44zo54wgV4121d69X');
+(7, 'admin', '4geN6r33VX19X3112');
 
 --
 -- Indexes for dumped tables
@@ -95,6 +122,12 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user_school_visit`
+--
+ALTER TABLE `user_school_visit`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user_token`
 --
 ALTER TABLE `user_token`
@@ -109,6 +142,12 @@ ALTER TABLE `user_token`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user_school_visit`
+--
+ALTER TABLE `user_school_visit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user_token`
